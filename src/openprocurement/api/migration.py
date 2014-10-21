5ac9297fecb60c9353f3c8602df223968d89ab48
Roman Kozlovskyi <krzroman@gmail.com>
2014-10-21 17:20:06 +0300

    Added schema version and migration procedure, pep8

--- src/openprocurement/api/migration.py
+++ src/openprocurement/api/migration.py
@@ -1,6 +1,36 @@
 # -*- coding: utf-8 -*-
+import logging
+
+
+LOGGER = logging.getLogger(__name__)
+SCHEMA_VERSION = 1
+SCHEMA_DOC = 'openprocurement_schema'
+
+
+def get_db_schema_version(db):
+    schema_doc = db.get(SCHEMA_DOC, {"_id": SCHEMA_DOC})
+    return schema_doc.get("version", 0)
+
+
+def set_db_schema_version(db, version):
+    schema_doc = db.get(SCHEMA_DOC, {"_id": SCHEMA_DOC})
+    schema_doc["version"] = version
+    db.save(schema_doc)
+
 
 def migrate_data(db):
+    cur_version = get_db_schema_version(db)
+    if cur_version == SCHEMA_VERSION:
+        return
+    for step in xrange(cur_version, SCHEMA_VERSION):
+        LOGGER.info("Migrate openprocurement schema from {} to {}".format(step, step + 1))
+        migration_func = globals().get('from{}to{}'.format(step, step + 1))
+        if migration_func:
+            migration_func(db)
+        set_db_schema_version(db, step + 1)
+
+
+def from0to1(db):
     results = db.view('tenders/all', include_docs=True)
     for i in results:
         doc = i.doc
--- src/openprocurement/api/tests.py
+++ src/openprocurement/api/tests.py
@@ -5,7 +5,7 @@ import os
 
 from openprocurement.api import VERSION
 from openprocurement.api.models import TenderDocument
-from openprocurement.api.migration import migrate_data
+from openprocurement.api.migration import migrate_data, get_db_schema_version, set_db_schema_version, SCHEMA_VERSION
 
 
 class PrefixedRequestClass(webtest.app.TestRequest):
@@ -23,7 +23,8 @@ class BaseWebTest(unittest.TestCase):
     """
 
     def setUp(self):
-        self.app = webtest.TestApp("config:tests.ini", relative_to=os.path.dirname(__file__))
+        self.app = webtest.TestApp(
+            "config:tests.ini", relative_to=os.path.dirname(__file__))
         self.app.RequestClass = PrefixedRequestClass
         self.couchdb_server = self.app.app.registry.couchdb_server
         self.db = self.app.app.registry.db
@@ -66,7 +67,12 @@ class SporeTest(BaseWebTest):
 class MigrateTest(BaseWebTest):
 
     def test_migrate(self):
-        data = {'doc_type': 'TenderDocument', 'modifiedAt': '2014-10-15T00:00:00.000000'}
+        self.assertEqual(get_db_schema_version(self.db), SCHEMA_VERSION)
+
+    def test_migrate_from0to1(self):
+        set_db_schema_version(self.db, 0)
+        data = {'doc_type': 'TenderDocument',
+                'modifiedAt': '2014-10-15T00:00:00.000000'}
         _id, _rev = self.db.save(data)
         item = self.db.get(_id)
         migrate_data(self.db)
@@ -106,15 +112,18 @@ class TenderResourceTest(BaseWebTest):
         self.assertEqual(response.content_type, 'application/json')
         self.assertEqual(response.json['status'], 'error')
         self.assertEqual(response.json['errors'], [
-            {u'description': u"Content-Type header should be one of ['application/json']", u'location': u'header', u'name': u'Content-Type'}
+            {u'description':
+                u"Content-Type header should be one of ['application/json']", u'location': u'header', u'name': u'Content-Type'}
         ])
 
-        response = self.app.post('/tenders', 'data', content_type='application/json', status=422)
+        response = self.app.post(
+            '/tenders', 'data', content_type='application/json', status=422)
         self.assertEqual(response.status, '422 Unprocessable Entity')
         self.assertEqual(response.content_type, 'application/json')
         self.assertEqual(response.json['status'], 'error')
         self.assertEqual(response.json['errors'], [
-            {u'description': u'No JSON object could be decoded', u'location': u'body', u'name': u'data'}
+            {u'description': u'No JSON object could be decoded',
+                u'location': u'body', u'name': u'data'}
         ])
 
         response = self.app.post_json('/tenders', 'data', status=422)
@@ -122,7 +131,8 @@ class TenderResourceTest(BaseWebTest):
         self.assertEqual(response.content_type, 'application/json')
         self.assertEqual(response.json['status'], 'error')
         self.assertEqual(response.json['errors'], [
-            {u'description': u'Data not available', u'location': u'body', u'name': u'data'}
+            {u'description': u'Data not available',
+                u'location': u'body', u'name': u'data'}
         ])
 
         response = self.app.post_json('/tenders', {'not_data': {}}, status=422)
@@ -130,31 +140,38 @@ class TenderResourceTest(BaseWebTest):
         self.assertEqual(response.content_type, 'application/json')
         self.assertEqual(response.json['status'], 'error')
         self.assertEqual(response.json['errors'], [
-            {u'description': u'Data not available', u'location': u'body', u'name': u'data'}
+            {u'description': u'Data not available',
+                u'location': u'body', u'name': u'data'}
         ])
 
-        response = self.app.post_json('/tenders', {'data': {'invalid_field': 'invalid_value'}}, status=422)
+        response = self.app.post_json('/tenders', {'data': {
+                                      'invalid_field': 'invalid_value'}}, status=422)
         self.assertEqual(response.status, '422 Unprocessable Entity')
         self.assertEqual(response.content_type, 'application/json')
         self.assertEqual(response.json['status'], 'error')
         self.assertEqual(response.json['errors'], [
-            {u'description': u'Rogue field', u'location': u'body', u'name': u'invalid_field'}
+            {u'description': u'Rogue field', u'location':
+                u'body', u'name': u'invalid_field'}
         ])
 
-        response = self.app.post_json('/tenders', {'data': {'totalValue': 'invalid_value'}}, status=422)
+        response = self.app.post_json('/tenders', {'data': {
+                                      'totalValue': 'invalid_value'}}, status=422)
         self.assertEqual(response.status, '422 Unprocessable Entity')
         self.assertEqual(response.content_type, 'application/json')
         self.assertEqual(response.json['status'], 'error')
         self.assertEqual(response.json['errors'], [
-            {u'description': [u'Please use a mapping for this field or Value instance instead of unicode.'], u'location': u'body', u'name': u'totalValue'}
+            {u'description': [
+                u'Please use a mapping for this field or Value instance instead of unicode.'], u'location': u'body', u'name': u'totalValue'}
         ])
 
-        response = self.app.post_json('/tenders', {'data': {'method': 'invalid_value'}}, status=422)
+        response = self.app.post_json('/tenders', {
+                                      'data': {'method': 'invalid_value'}}, status=422)
         self.assertEqual(response.status, '422 Unprocessable Entity')
         self.assertEqual(response.content_type, 'application/json')
         self.assertEqual(response.json['status'], 'error')
         self.assertEqual(response.json['errors'], [
-            {u'description': [u"Value must be one of ['Open', 'Selective', 'Limited']."], u'location': u'body', u'name': u'method'}
+            {u'description': [
+                u"Value must be one of ['Open', 'Selective', 'Limited']."], u'location': u'body', u'name': u'method'}
         ])
 
     def test_create_tender_generated(self):
@@ -176,42 +193,42 @@ class TenderResourceTest(BaseWebTest):
         data = {
             "procuringEntity": {
                 "id": {
-                    "name":"Державне управління справами",
-                    "scheme":"https://ns.openprocurement.org/ua/edrpou",
-                    "uid":"00037256",
-                    "uri":"http://www.dus.gov.ua/"
+                    "name": "Державне управління справами",
+                    "scheme": "https://ns.openprocurement.org/ua/edrpou",
+                    "uid": "00037256",
+                    "uri": "http://www.dus.gov.ua/"
                 },
                 "address": {
-                    "country-name":"Україна",
-                    "postal-code":"01220",
-                    "region":"м. Київ",
-                    "locality":"м. Київ",
-                    "street-address":" вул. Банкова, 11, корпус 1"
+                    "country-name": "Україна",
+                    "postal-code": "01220",
+                    "region": "м. Київ",
+                    "locality": "м. Київ",
+                    "street-address": " вул. Банкова, 11, корпус 1"
                 },
             },
             "totalValue": {
-                "amount":500,
-                "currency":"UAH"
+                "amount": 500,
+                "currency": "UAH"
             },
             "itemsToBeProcured": [
                 {
-                    "description":"футляри до державних нагород",
-                    "classificationScheme":"Other",
-                    "otherClassificationScheme":"ДКПП",
-                    "classificationID":"17.21.1",
-                    "classificationDescription":"папір і картон гофровані, паперова й картонна тара",
-                    "unitOfMeasure":"item",
-                    "quantity":5
+                    "description": "футляри до державних нагород",
+                    "classificationScheme": "Other",
+                    "otherClassificationScheme": "ДКПП",
+                    "classificationID": "17.21.1",
+                    "classificationDescription": "папір і картон гофровані, паперова й картонна тара",
+                    "unitOfMeasure": "item",
+                    "quantity": 5
                 }
             ],
             "clarificationPeriod": {
-                "endDate":"2014-10-31T00:00:00"
+                "endDate": "2014-10-31T00:00:00"
             },
             "tenderPeriod": {
-                "endDate":"2014-11-06T10:00:00"
+                "endDate": "2014-11-06T10:00:00"
             },
             "awardPeriod": {
-                "endDate":"2014-11-13T00:00:00"
+                "endDate": "2014-11-13T00:00:00"
             }
         }
 
@@ -219,7 +236,8 @@ class TenderResourceTest(BaseWebTest):
         self.assertEqual(response.status, '201 Created')
         self.assertEqual(response.content_type, 'application/json')
         tender = response.json['data']
-        self.assertEqual(set(tender) - set(data), set([u'id', u'modified', u'tenderID']))
+        self.assertEqual(set(tender) - set(data), set(
+            [u'id', u'modified', u'tenderID']))
 
         response = self.app.get('/tenders/{}'.format(tender['id']))
         self.assertEqual(response.status, '200 OK')
@@ -251,7 +269,8 @@ class TenderResourceTest(BaseWebTest):
         tender['method'] = 'Open'
         modified = tender.pop('modified')
 
-        response = self.app.put_json('/tenders/{}'.format(tender['id']), {'data': tender})
+        response = self.app.put_json('/tenders/{}'.format(
+            tender['id']), {'data': tender})
         self.assertEqual(response.status, '200 OK')
         self.assertEqual(response.content_type, 'application/json')
         new_tender = response.json['data']
@@ -269,7 +288,8 @@ class TenderResourceTest(BaseWebTest):
         tender = response.json['data']
         modified = tender.pop('modified')
 
-        response = self.app.patch_json('/tenders/{}'.format(tender['id']), {'data': {'method': 'Open'}})
+        response = self.app.patch_json('/tenders/{}'.format(
+            tender['id']), {'data': {'method': 'Open'}})
         self.assertEqual(response.status, '200 OK')
         self.assertEqual(response.content_type, 'application/json')
         new_tender = response.json['data']
@@ -293,7 +313,8 @@ class TenderResourceTest(BaseWebTest):
         self.assertEqual(response.content_type, 'application/json')
         self.assertEqual(response.json['data']['modified'], modified)
 
-        response = self.app.patch_json('/tenders/{}'.format(tender['id']), {'data': {'method': 'Open'}})
+        response = self.app.patch_json('/tenders/{}'.format(
+            tender['id']), {'data': {'method': 'Open'}})
         self.assertEqual(response.status, '200 OK')
         self.assertEqual(response.content_type, 'application/json')
         self.assertNotEqual(response.json['data']['modified'], modified)
@@ -319,7 +340,8 @@ class TenderResourceTest(BaseWebTest):
             {u'description': u'Not Found', u'location': u'url', u'name': u'id'}
         ])
 
-        response = self.app.put_json('/tenders/some_id', {'data': {}}, status=404)
+        response = self.app.put_json(
+            '/tenders/some_id', {'data': {}}, status=404)
         self.assertEqual(response.status, '404 Not Found')
         self.assertEqual(response.content_type, 'application/json')
         self.assertEqual(response.json['status'], 'error')
@@ -327,13 +349,15 @@ class TenderResourceTest(BaseWebTest):
             {u'description': u'Not Found', u'location': u'url', u'name': u'id'}
         ])
 
-        response = self.app.patch_json('/tenders/some_id', {'data': {}}, status=404)
+        response = self.app.patch_json(
+            '/tenders/some_id', {'data': {}}, status=404)
         self.assertEqual(response.status, '404 Not Found')
         self.assertEqual(response.content_type, 'application/json')
         self.assertEqual(response.json['status'], 'error')
         self.assertEqual(response.json['errors'], [
             {u'description': u'Not Found', u'location': u'url', u'name': u'id'}
         ])
+
 
 class TenderDocumentResourceTest(BaseWebTest):
 
@@ -360,29 +384,35 @@ class TenderDocumentResourceTest(BaseWebTest):
         self.assertEqual(response.content_type, 'application/json')
         self.assertEqual(response.json['status'], 'error')
         self.assertEqual(response.json['errors'], [
-            {u'description': u'Not Found', u'location': u'url', u'name': u'tender_id'}
+            {u'description': u'Not Found', u'location':
+                u'url', u'name': u'tender_id'}
         ])
 
     def test_post_tender_not_found(self):
-        response = self.app.post('/tenders/some_id/documents', status=404, upload_files=[('upload', 'name.doc', 'content')])
+        response = self.app.post('/tenders/some_id/documents', status=404, upload_files=[
+                                 ('upload', 'name.doc', 'content')])
         self.assertEqual(response.status, '404 Not Found')
         self.assertEqual(response.content_type, 'application/json')
         self.assertEqual(response.json['status'], 'error')
         self.assertEqual(response.json['errors'], [
-            {u'description': u'Not Found', u'location': u'url', u'name': u'tender_id'}
+            {u'description': u'Not Found', u'location':
+                u'url', u'name': u'tender_id'}
         ])
 
     def test_put_tender_not_found(self):
-        response = self.app.put('/tenders/some_id/documents/some_id', status=404, upload_files=[('upload', 'name.doc', 'content2')])
+        response = self.app.put('/tenders/some_id/documents/some_id', status=404, upload_files=[
+                                ('upload', 'name.doc', 'content2')])
         self.assertEqual(response.status, '404 Not Found')
         self.assertEqual(response.content_type, 'application/json')
         self.assertEqual(response.json['status'], 'error')
         self.assertEqual(response.json['errors'], [
-            {u'description': u'Not Found', u'location': u'url', u'name': u'tender_id'}
+            {u'description': u'Not Found', u'location':
+                u'url', u'name': u'tender_id'}
         ])
 
     def test_put_tender_document_not_found(self):
-        response = self.app.put('/tenders/{}/documents/some_id'.format(self.tender_id), status=404, upload_files=[('upload', 'name.doc', 'content2')])
+        response = self.app.put('/tenders/{}/documents/some_id'.format(
+            self.tender_id), status=404, upload_files=[('upload', 'name.doc', 'content2')])
         self.assertEqual(response.status, '404 Not Found')
         self.assertEqual(response.content_type, 'application/json')
         self.assertEqual(response.json['status'], 'error')
@@ -391,7 +421,8 @@ class TenderDocumentResourceTest(BaseWebTest):
         ])
 
     def test_get_tender_document_not_found(self):
-        response = self.app.get('/tenders/{}/documents/some_id'.format(self.tender_id), status=404)
+        response = self.app.get('/tenders/{}/documents/some_id'.format(
+            self.tender_id), status=404)
         self.assertEqual(response.status, '404 Not Found')
         self.assertEqual(response.content_type, 'application/json')
         self.assertEqual(response.json['status'], 'error')
@@ -400,29 +431,34 @@ class TenderDocumentResourceTest(BaseWebTest):
         ])
 
     def test_create_tender_document(self):
-        response = self.app.post('/tenders/{}/documents'.format(self.tender_id), upload_files=[('upload', 'name.doc', 'content')])
+        response = self.app.post('/tenders/{}/documents'.format(
+            self.tender_id), upload_files=[('upload', 'name.doc', 'content')])
         self.assertEqual(response.status, '201 Created')
         self.assertEqual(response.content_type, 'application/json')
         self.assertTrue('name.doc' in response.json["documents"])
 
-        response = self.app.get('/tenders/{}/documents'.format(self.tender_id, 'name.doc'))
+        response = self.app.get('/tenders/{}/documents'.format(
+            self.tender_id, 'name.doc'))
         self.assertEqual(response.status, '200 OK')
         self.assertEqual(response.content_type, 'application/json')
         self.assertTrue('name.doc' in response.json["documents"])
 
-        response = self.app.get('/tenders/{}/documents/{}'.format(self.tender_id, 'name.doc'))
+        response = self.app.get('/tenders/{}/documents/{}'.format(
+            self.tender_id, 'name.doc'))
         self.assertEqual(response.status, '200 OK')
         self.assertEqual(response.content_type, 'text/html')
         self.assertEqual(response.content_length, 7)
         self.assertEqual(response.body, 'content')
 
     def test_put_tender_document(self):
-        response = self.app.post('/tenders/{}/documents'.format(self.tender_id), upload_files=[('upload', 'name.doc', 'content')])
+        response = self.app.post('/tenders/{}/documents'.format(
+            self.tender_id), upload_files=[('upload', 'name.doc', 'content')])
         self.assertEqual(response.status, '201 Created')
         self.assertEqual(response.content_type, 'application/json')
         self.assertTrue('name.doc' in response.json["documents"])
 
-        response = self.app.put('/tenders/{}/documents/{}'.format(self.tender_id, 'name.doc'), upload_files=[('upload', 'name.doc', 'content2')])
+        response = self.app.put('/tenders/{}/documents/{}'.format(
+            self.tender_id, 'name.doc'), upload_files=[('upload', 'name.doc', 'content2')])
         self.assertEqual(response.status, '200 OK')
         self.assertEqual(response.content_type, 'application/json')
         self.assertEqual(response.json["content_type"], 'application/msword')
