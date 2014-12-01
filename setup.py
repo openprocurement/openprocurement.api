c5653a1bc6590fb6011a835c20f1fe9f8eb654c8
Myroslav Opyr <myroslav@quintagroup.com>
2014-12-01 17:56:49 +0200

    Preparing API v0.4 release. Cont

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ main = openprocurement.api:main
 """
 
 setup(name='openprocurement.api',
-      version='0.3',
+      version='0.4',
       description='openprocurement.api',
       long_description=README,
       classifiers=[
