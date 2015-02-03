e5bd873907ad658b74fce6c80676b3cfa03aa8e4
Roman Kozlovskyi <krzroman@gmail.com>
2015-02-03 12:08:32 +0200

    Added CLIENT_REQUEST_ID

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ main = openprocurement.api:main
 """
 
 setup(name='openprocurement.api',
-      version='0.6.1',
+      version='0.6.2',
       description='openprocurement.api',
       long_description=README,
       classifiers=[
