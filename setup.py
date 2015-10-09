70f90bafd6d64a7a0f56157624818ba9670a4985
Roman Kozlovskyi <krzroman@gmail.com>
2015-10-09 18:24:30 +0300

    Version up

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ main = openprocurement.api:main
 """
 
 setup(name='openprocurement.api',
-      version='0.9',
+      version='0.10',
       description='openprocurement.api',
       long_description=README,
       classifiers=[
