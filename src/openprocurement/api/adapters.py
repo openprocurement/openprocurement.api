397d7a7fb571ebeb32453069756f2087a2ba0326
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2015-11-19 18:14:28 +0200

    remove unused adapter

--- src/openprocurement/api/adapters.py
+++ src/openprocurement/api/adapters.py
@@ -21,35 +21,6 @@ class makeTenderUA(makeBaseTender):
     model = TenderUA
 
 
-class loadTender(object):
-    def __init__(self, request, tid):
-        self.request = request
-        self.tid = tid
-
-    def _tenderAdapter(self):
-        db = self.request.registry.db
-        doc = db.get(self.tid)
-        if doc is None:
-            return None
-
-        return self.request.registry.queryAdapter(doc, IBaseTender,
-                                                  name=doc.get('subtype',
-                                                               'Tender'))
-
-    def tender(self):
-        adapter = self._tenderAdapter()
-        if adapter is None:
-            return None
-        return adapter.tender()
-
-    @property
-    def model(self):
-        adapter = self._tenderAdapter()
-        if adapter is None:
-            return None
-        return adapter.model
-
-
 @subscriber(ApplicationCreated)
 def register_adapters(event):
     registry = event.app.registry
@@ -57,5 +28,3 @@ def register_adapters(event):
     registry.registerAdapter(makeTender, (dict, ), ITender, name='Tender')
     registry.registerAdapter(makeTenderUA, (dict, ), ITenderUA,
                              name='TenderUA')
-
-    registry.registerAdapter(loadTender, (Request, unicode), IBaseTender)
