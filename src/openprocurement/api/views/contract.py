737de65ecb6a192b1f8f3d2f55be58976354d9b8
Volodymyr Vozniak <volvoz@quintagroup.org>
2016-02-03 19:04:31 +0200

    update contract fix

--- src/openprocurement/api/views/contract.py
+++ src/openprocurement/api/views/contract.py
@@ -33,7 +33,7 @@ class TenderAwardContractResource(object):
         """Post a contract for award
         """
         tender = self.request.validated['tender']
-        if tender.status not in ['active.qualification', 'active.awarded', 'complete']:
+        if tender.status not in ['active.qualification', 'active.awarded']:
             self.request.errors.add('body', 'data', 'Can\'t add contract in current ({}) tender status'.format(tender.status))
             self.request.errors.status = 403
             return
@@ -64,7 +64,7 @@ class TenderAwardContractResource(object):
     def patch(self):
         """Update of contract
         """
-        if self.request.validated['tender_status'] not in ['active.qualification', 'active.awarded', 'complete']:
+        if self.request.validated['tender_status'] not in ['active.qualification', 'active.awarded']:
             self.request.errors.add('body', 'data', 'Can\'t update contract in current ({}) tender status'.format(self.request.validated['tender_status']))
             self.request.errors.status = 403
             return
