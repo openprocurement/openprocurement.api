63879077eea692fb42e553e12c05e94d4f2dd01d
Taras Kozlovskyi <ktarasz@quintagroup.com>
2016-02-24 15:30:30 +0200

    Approve health view

--- src/openprocurement/api/views/health.py
+++ src/openprocurement/api/views/health.py
@@ -2,16 +2,14 @@
 from cornice.service import Service
 from pyramid.response import Response
 
-
 health = Service(name='health', path='/health', renderer='json')
 
 
 @health.get()
 def get_spore(request):
     tasks = request.registry.admin_couchdb_server.tasks()
-    request.errors.status = 503
-    if not(tasks and all([True
-                          for task in tasks
-                          if 'progress' in task and task['progress'] > request.registry.health_threshold])):
-        return Response(json_body=tasks, status=503)
-    return tasks
+    output = {task['replication_id']:task['progress'] for task in tasks if 'type' in task and task['type'] == 'replication'}
+    if not(tasks and all([True if progress > request.registry.health_threshold else False
+                          for progress in tasks.values()])):
+        return Response(json_body=output, status=503)
+    return output
