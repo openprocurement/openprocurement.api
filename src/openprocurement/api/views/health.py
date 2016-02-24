463913d39ffd8d789738f83b4a84aac9169b12cd
Taras Kozlovskyi <ktarasz@quintagroup.com>
2016-02-24 16:18:30 +0200

    Fixed health view

--- src/openprocurement/api/views/health.py
+++ src/openprocurement/api/views/health.py
@@ -9,7 +9,7 @@ health = Service(name='health', path='/health', renderer='json')
 def get_spore(request):
     tasks = getattr(request.registry, 'admin_couchdb_server', request.registry.couchdb_server).tasks()
     output = {task['replication_id']:task['progress'] for task in tasks if 'type' in task and task['type'] == 'replication'}
-    if not(tasks and all([True if progress > request.registry.health_threshold else False
-                          for progress in tasks.values()])):
+    if not(output and all([True if progress > request.registry.health_threshold else False
+                          for progress in output.values()])):
         return Response(json_body=output, status=503)
     return output
