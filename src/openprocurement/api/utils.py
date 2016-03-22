50d9af28848a5d258be0c235f6a198fb9a0abb3c
Roman Kozlovskyi <krzroman@gmail.com>
2016-03-22 18:05:35 +0200

    Fixed calculation datetime delta with working days

--- src/openprocurement/api/utils.py
+++ src/openprocurement/api/utils.py
@@ -792,19 +792,19 @@ def calculate_business_date(date_obj, timedelta_obj, context=None, working_days=
     if working_days:
         date = date_obj
         if timedelta_obj > timedelta():
-            if date.weekday() in [5, 6] and WORKING_DAYS.get(date.date().isoformat(), True) or WORKING_DAYS.get(date.date().isoformat(), False):
-                date = datetime.combine(date.date(), time(0, tzinfo=date.tzinfo)) + timedelta(1)
-                while date.weekday() in [5, 6] and WORKING_DAYS.get(date.date().isoformat(), True) or WORKING_DAYS.get(date.date().isoformat(), False):
-                    date += timedelta(1)
+            if date_obj.weekday() in [5, 6] and WORKING_DAYS.get(date_obj.date().isoformat(), True) or WORKING_DAYS.get(date_obj.date().isoformat(), False):
+                date_obj = datetime.combine(date_obj.date(), time(0, tzinfo=date_obj.tzinfo)) + timedelta(1)
+                while date_obj.weekday() in [5, 6] and WORKING_DAYS.get(date_obj.date().isoformat(), True) or WORKING_DAYS.get(date_obj.date().isoformat(), False):
+                    date_obj += timedelta(1)
         else:
-            if date.weekday() in [5, 6] and WORKING_DAYS.get(date.date().isoformat(), True) or WORKING_DAYS.get(date.date().isoformat(), False):
-                date = datetime.combine(date.date(), time(0, tzinfo=date.tzinfo))
-                while date.weekday() in [5, 6] and WORKING_DAYS.get(date.date().isoformat(), True) or WORKING_DAYS.get(date.date().isoformat(), False):
-                    date -= timedelta(1)
-                date += timedelta(1)
+            if date_obj.weekday() in [5, 6] and WORKING_DAYS.get(date_obj.date().isoformat(), True) or WORKING_DAYS.get(date_obj.date().isoformat(), False):
+                date_obj = datetime.combine(date_obj.date(), time(0, tzinfo=date_obj.tzinfo))
+                while date_obj.weekday() in [5, 6] and WORKING_DAYS.get(date_obj.date().isoformat(), True) or WORKING_DAYS.get(date_obj.date().isoformat(), False):
+                    date_obj -= timedelta(1)
+                date_obj += timedelta(1)
         for _ in xrange(abs(timedelta_obj.days)):
-            date += timedelta(1) if timedelta_obj > timedelta() else -timedelta(1)
-            while date.weekday() in [5, 6] and WORKING_DAYS.get(date.date().isoformat(), True) or WORKING_DAYS.get(date.date().isoformat(), False):
-                date += timedelta(1) if timedelta_obj > timedelta() else -timedelta(1)
-        timedelta_obj = date - date_obj
+            date_obj += timedelta(1) if timedelta_obj > timedelta() else -timedelta(1)
+            while date_obj.weekday() in [5, 6] and WORKING_DAYS.get(date_obj.date().isoformat(), True) or WORKING_DAYS.get(date_obj.date().isoformat(), False):
+                date_obj += timedelta(1) if timedelta_obj > timedelta() else -timedelta(1)
+        return date_obj
     return date_obj + timedelta_obj
