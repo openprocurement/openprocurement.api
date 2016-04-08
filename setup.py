cf181e7cc6c4c144f743296a121d8c9f1e86d932
Roman Kozlovskyi <krzroman@gmail.com>
2016-04-08 13:49:20 +0300

    Testing additionalClassifications reorder

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ entry_points = {
 }
 
 setup(name='openprocurement.api',
-      version='2.2',
+      version='2.2.1',
       description='openprocurement.api',
       long_description=README,
       classifiers=[
