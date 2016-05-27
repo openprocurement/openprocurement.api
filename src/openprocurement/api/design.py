f20d6c83a39341783e7b095df33c5a53f3f2308f
oleh <chezar1995@gmail.com>
2016-05-27 12:28:00 +0300

    push all design views to list.

--- src/openprocurement/api/design.py
+++ src/openprocurement/api/design.py
@@ -34,11 +34,6 @@ def add_index_options(doc):
     doc['options'] = {'local_seq': True}
 
 
-def sync_design(db):
-    views = [j for i, j in globals().items() if "_view" in i]
-    ViewDefinition.sync_many(db, views, callback=add_index_options)
-
-
 tenders_all_view = ViewDefinition('tenders', 'all', '''function(doc) {
     if(doc.doc_type == 'Tender') {
         emit(doc.tenderID, null);
@@ -123,3 +118,13 @@ conflicts_view = ViewDefinition('conflicts', 'all', '''function(doc) {
         emit(doc._rev, [doc._rev].concat(doc._conflicts));
     }
 }''')
+
+
+design_list = [tenders_all_view, tenders_by_dateModified_view, tenders_real_by_dateModified_view,
+               tenders_test_by_dateModified_view, tenders_by_local_seq_view,
+               tenders_real_by_local_seq_view, tenders_test_by_local_seq_view,
+               conflicts_view]
+
+
+def sync_design(db):
+    ViewDefinition.sync_many(db, design_list, callback=add_index_options)
