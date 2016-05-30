33a7658a552cc430d40ff4e490132a17590faa9b
Taras Kozlovskyi <ktarasz@quintagroup.com>
2016-05-30 18:34:29 +0300

    Prepare openprocurement.api 2.3.3.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ entry_points = {
 }
 
 setup(name='openprocurement.api',
-      version='2.3.2',
+      version='2.3.3',
       description='openprocurement.api',
       long_description=README,
       classifiers=[
