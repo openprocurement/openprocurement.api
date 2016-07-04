511888c512d68d5f8f7dcc49dc6e3034c671a8c0
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-07-04 16:04:25 +0300

    generate transfer for complaints

--- src/openprocurement/api/views/award_complaint.py
+++ src/openprocurement/api/views/award_complaint.py
@@ -4,6 +4,7 @@ from openprocurement.api.utils import (
     apply_patch,
     check_tender_status,
     context_unpack,
+    generate_id,
     json_view,
     opresource,
     save_tender,
@@ -49,6 +50,8 @@ class TenderAwardComplaintResource(APIResource):
         else:
             complaint.status = 'draft'
         complaint.complaintID = '{}.{}{}'.format(tender.tenderID, self.server_id, sum([len(i.complaints) for i in tender.awards], len(tender.complaints)) + 1)
+        transfer = generate_id()
+        complaint.transfer_token = transfer
         set_ownership(complaint, self.request)
         self.context.complaints.append(complaint)
         if save_tender(self.request):
@@ -59,7 +62,8 @@ class TenderAwardComplaintResource(APIResource):
             return {
                 'data': complaint.serialize("view"),
                 'access': {
-                    'token': complaint.owner_token
+                    'token': complaint.owner_token,
+                    'transfer': transfer
                 }
             }
 
