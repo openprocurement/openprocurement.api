bcf8608764612dd63c932ca19a6cf7dbbd975d85
ivan1233211 <ivanrouman@gmail.com>
2016-11-07 16:37:41 +0200

    Try evoid merge conflict

--- src/openprocurement/api/models.py
+++ src/openprocurement/api/models.py
@@ -361,7 +361,7 @@ class Item(Model):
 class Document(Model):
     class Options:
         roles = {
-            'edit': blacklist('id', 'url', 'datePublished', 'dateModified', 'sikret_key', ''),
+            'edit': blacklist('id', 'url', 'datePublished', 'dateModified', 'sikret_key'),
             'embedded': schematics_embedded_role,
             'view': (blacklist('revisions') + schematics_default_role),
             'revisions': whitelist('url', 'dateModified'),
