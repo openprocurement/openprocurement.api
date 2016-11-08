10a675e11e715ac610a7532393cbb7b58457f047
Roman Kozlovskyi <krzroman@gmail.com>
2016-11-08 15:54:13 +0200

    Prepare openprocurement.api 2.3.18+px.4.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ entry_points = {
 }
 
 setup(name='openprocurement.api',
-      version='2.3.18+px.3',
+      version='2.3.18+px.4',
       description='openprocurement.api',
       long_description=README,
       classifiers=[
