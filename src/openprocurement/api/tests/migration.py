937f1269601c500412cd091294c25962307b4809
Yaroslav Neznaradko <yarsanich@quintagroup.org>
2017-03-15 18:50:01 +0200

    minimal tests to migrations

---
+++ src/openprocurement/api/tests/migration.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from openprocurement.api.migration import (
+    migrate_data, get_db_schema_version
+)
+from openprocurement.api.constants import (
+    SCHEMA_VERSION
+)
+from openprocurement.api.tests.base import BaseWebTest
+
+
+class MigrateTest(BaseWebTest):
+
+    def setUp(self):
+        super(MigrateTest, self).setUp()
+        migrate_data(self.app.app.registry)
+
+    def test_migrate(self):
+        self.assertEqual(get_db_schema_version(self.db), SCHEMA_VERSION)
+        migrate_data(self.app.app.registry, 1)
+        self.assertEqual(get_db_schema_version(self.db), SCHEMA_VERSION)
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(MigrateTest))
+    return suite
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
