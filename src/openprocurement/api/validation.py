2a4678eb48799694379b78360cdd849f011d8648
annawzz <annawzz@quintagroup.org>
2017-03-23 17:16:04 +0200

    Add raise error_handler in validators

--- src/openprocurement/api/validation.py
+++ src/openprocurement/api/validation.py
@@ -3,6 +3,7 @@ from schematics.exceptions import (
     ModelValidationError, ModelConversionError, ValidationError
 )
 from openprocurement.api.utils import apply_data_patch, update_logging_context
+from openprocurement.tender.core.utils import error_handler
 
 
 def validate_json_data(request):
@@ -11,11 +12,11 @@ def validate_json_data(request):
     except ValueError, e:
         request.errors.add('body', 'data', e.message)
         request.errors.status = 422
-        return
+        raise error_handler(request.errors)
     if not isinstance(json, dict) or 'data' not in json or not isinstance(json.get('data'), dict):
         request.errors.add('body', 'data', "Data not available")
         request.errors.status = 422
-        return
+        raise error_handler(request.errors)
     request.validated['json_data'] = json['data']
     return json['data']
 
@@ -23,8 +24,6 @@ def validate_json_data(request):
 def validate_data(request, model, partial=False, data=None):
     if data is None:
         data = validate_json_data(request)
-    if data is None:
-        return
     try:
         if partial and isinstance(request.context, model):
             initial_data = request.context.serialize()
@@ -46,16 +45,16 @@ def validate_data(request, model, partial=False, data=None):
         for i in e.message:
             request.errors.add('body', i, e.message[i])
         request.errors.status = 422
-        data = None
+        raise error_handler(request.errors)
     except ValueError, e:
         request.errors.add('body', 'data', e.message)
         request.errors.status = 422
-        data = None
+        raise error_handler(request.errors)
     else:
         if hasattr(type(m), '_options') and role not in type(m)._options.roles:
             request.errors.add('url', 'role', 'Forbidden')
             request.errors.status = 403
-            data = None
+            raise error_handler(request.errors)
         else:
             data = method(role)
             request.validated['data'] = data
