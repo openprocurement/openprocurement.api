1c1c7705d64420c0db53677fcd96790e792e811a
Oleksandr <oleksanr.v@gmail.com>
2017-03-27 17:06:39 +0300

    Add asymmetric encryption (#169)

--- src/openprocurement/api/__init__.py
+++ src/openprocurement/api/__init__.py
@@ -9,6 +9,7 @@ from base64 import b64decode, b64encode
 from couchdb import Server as CouchdbServer, Session
 from couchdb.http import Unauthorized, extract_credentials
 from libnacl.sign import Signer, Verifier
+from libnacl.public import SecretKey, PublicKey
 from logging import getLogger
 from openprocurement.api.auth import AuthenticationPolicy, authenticated_role, check_accreditation
 from openprocurement.api.design import sync_design
@@ -168,6 +169,10 @@ def main(global_config, **settings):
     dockeys = settings.get('dockeys') if 'dockeys' in settings else dockey.hex_vk()
     for key in dockeys.split('\0'):
         keyring[key[:8]] = Verifier(key)
+
+    # Archive keys
+    arch_pubkey = settings.get('arch_pubkey', None)
+    config.registry.arch_pubkey = PublicKey(arch_pubkey.decode('hex') if arch_pubkey else SecretKey().pk)
 
     # migrate data
     if not os.environ.get('MIGRATION_SKIP'):
