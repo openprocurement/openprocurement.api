e41c5f107b57372230b27c14fe5496e56214a670
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2017-04-13 14:51:58 +0300

    prepare 2.3.54

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ entry_points = {
 }
 
 setup(name='openprocurement.api',
-      version='2.3.52',
+      version='2.3.54',
       description='openprocurement.api',
       long_description=README,
       classifiers=[
