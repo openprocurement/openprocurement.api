c5dc1fd4a88aa8ff9b563c338b7c3f0ac1a77987
Yaroslav Neznaradko <yarsanich@quintagroup.org>
2017-05-03 17:52:22 +0300

    update doc string

--- src/openprocurement/api/tests/base.py
+++ src/openprocurement/api/tests/base.py
@@ -16,10 +16,13 @@ now = datetime.now()
 
 def snitch(func):
     """
-        snitch method get test without test_ part in title
-        This method return a copy of function with test_ part at begining
-        It provide a way to realize a storage for tests that are non-executable
-        and provide a way for import tests where it needed.
+        This method is used to add test function to TestCase classes.
+        snitch method gets test function and returns a copy of this function
+        with 'test_' prefix at the beginning (to identify this function as
+        an executable test).
+        It provides a way to implement a storage (python module that
+        contains non-executable test functions) for tests and to include
+        different set of functions into different test cases.
     """
     return FunctionType(func.func_code, func.func_globals,
                         'test_' + func.func_name, closure=func.func_closure)
