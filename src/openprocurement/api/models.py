e73d04c61cd5a4779bdae4f9144c4700dd4632b0
annawzz <annawzz@quintagroup.org>
2017-06-14 13:52:58 +0300

    Block CPV classification on refactoring

--- src/openprocurement/api/models.py
+++ src/openprocurement/api/models.py
@@ -15,9 +15,9 @@ from schematics.types.compound import (ModelType, DictType,
                                        ListType as BaseListType)
 from schematics.types.serializable import serializable
 
-from openprocurement.api.utils import get_now, set_parent
+from openprocurement.api.utils import get_now, set_parent, get_schematics_document
 from openprocurement.api.constants import (
-    CPV_CODES, ORA_CODES, TZ, DK_CODES
+    CPV_CODES, ORA_CODES, TZ, DK_CODES, CPV_BLOCK_FROM
 )
 
 schematics_default_role = SchematicsDocument.Options.roles['default'] + blacklist("__parent__")
@@ -226,6 +226,11 @@ class CPVClassification(Classification):
             raise ValidationError(BaseType.MESSAGES['choices'].format(unicode(CPV_CODES)))
         elif data.get('scheme') == u'ДК021' and code not in DK_CODES:
             raise ValidationError(BaseType.MESSAGES['choices'].format(unicode(DK_CODES)))
+
+    def validate_scheme(self, data, scheme):
+        schematics_document = get_schematics_document(data['__parent__'])
+        if (schematics_document.get('revisions')[0].date if schematics_document.get('revisions') else get_now()) > CPV_BLOCK_FROM and scheme != u'ДК021':
+            raise ValidationError(BaseType.MESSAGES['choices'].format(unicode([u'ДК021'])))
 
 
 class Unit(Model):
