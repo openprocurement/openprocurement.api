ea9520b34c176ed0a6031fefdd88c4c8ebb6bfda
Roman Kozlovskyi <kroman0@quintagroup.com>
2017-07-07 14:15:05 +0300

    Added new response for archived tenders

--- src/openprocurement/api/utils.py
+++ src/openprocurement/api/utils.py
@@ -812,7 +812,11 @@ def context_unpack(request, msg, params=None):
 def extract_tender_adapter(request, tender_id):
     db = request.registry.db
     doc = db.get(tender_id)
-    if doc is None or doc.get('doc_type') != 'Tender':
+    if doc is not None and doc.get('doc_type') == 'tender':
+        request.errors.add('url', 'tender_id', 'Archived')
+        request.errors.status = 410
+        raise error_handler(request.errors)
+    elif doc is None or doc.get('doc_type') != 'Tender':
         request.errors.add('url', 'tender_id', 'Not Found')
         request.errors.status = 404
         raise error_handler(request.errors)
