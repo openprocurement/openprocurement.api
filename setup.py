97b976f2bf8f660bc23d51ee78106f96e7495521
annawzz <annawzz@quintagroup.org>
2017-11-16 17:43:19 +0200

    Prepare openprocurement.api 2.4.4.

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ entry_points = {
 }
 
 setup(name='openprocurement.api',
-      version='2.4.3',
+      version='2.4.4',
       description='openprocurement.api',
       long_description=README,
       classifiers=[
