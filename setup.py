1f9b2c7836d6738d5a86d93644a04da7729b3688
Roman Kozlovskyi <kroman0@quintagroup.com>
2017-12-07 13:37:36 +0200

    Prepare openprocurement.api 2.4.5.

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ entry_points = {
 }
 
 setup(name='openprocurement.api',
-      version='2.4.4',
+      version='2.4.5',
       description='openprocurement.api',
       long_description=README,
       classifiers=[
