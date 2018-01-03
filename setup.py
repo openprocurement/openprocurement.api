c5be0392a7168144abcd1ae58cb9fc5964fed7cc
Andrew Leitsius <leitsius@gmail.com>
2018-01-03 11:02:31 +0200

    Prepare openprocurement.api 2.3.4-sale

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ entry_points = {
 }
 
 setup(name='openprocurement.api',
-      version='2.3.3-sale',
+      version='2.3.4-sale',
       description='openprocurement.api',
       long_description=README,
       classifiers=[
