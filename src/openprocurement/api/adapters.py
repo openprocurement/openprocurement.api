112bc95179f32455cdbd2ce56bd79cb242ff075f
yarsanich <nezn27@gmail.com>
2018-01-26 18:20:29 +0200

    add next_check interface and adapter

--- src/openprocurement/api/adapters.py
+++ src/openprocurement/api/adapters.py
@@ -10,3 +10,15 @@ class ContentConfigurator(object):
 
     def __repr__(self):
         return "<Configuration adapter for %s>" % type(self.context)
+
+
+class AwardingNextCheckAdapter(object):
+    """ Base Awarding Next Check Adapter"""
+
+    name = "Base Awarding Next Check Adapter"
+
+    def __init__(self, context):
+        self.context = context
+
+    def __repr__(self):
+        return "<Awarding next_check adapter for %s>" % type(self.context)
