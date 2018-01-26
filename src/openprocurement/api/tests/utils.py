786d84b9a93f9b8a608c6b308ae37d96867ceca3
yarsanich <nezn27@gmail.com>
2018-01-26 18:41:22 +0200

    add tests for get_request_from_root function

--- src/openprocurement/api/tests/utils.py
+++ src/openprocurement/api/tests/utils.py
@@ -1,12 +1,22 @@
-from unittest import TestCase
-
+from openprocurement.api.models import (
+    Tender,
+    Bid
+)
+from openprocurement.api.traversal import Root
 from openprocurement.api.constants import AWARDING_OF_PROCUREMENT_METHOD_TYPE
 from openprocurement.api.utils import (
-    get_awarding_type_by_procurement_method_type
+    get_awarding_type_by_procurement_method_type,
+    get_request_from_root
+)
+from openprocurement.api.tests.base import (
+    BaseWebTest,
+    test_tender_data,
+    RequestWithRegistry,
+    test_bids
 )
 
 
-class TestCoreUtils(TestCase):
+class TestCoreUtils(BaseWebTest):
 
     def test_get_awarding_type_by_procurement_method_type(self):
         for key in AWARDING_OF_PROCUREMENT_METHOD_TYPE.keys():
@@ -21,3 +31,31 @@ class TestCoreUtils(TestCase):
         with self.assertRaises(ValueError) as context:
             get_awarding_type_by_procurement_method_type('jdfvhdlkjv')
 
+    def test_get_request_from_root_with_request(self):
+        # Create tender with Tender model
+        test_tender = Tender(test_tender_data)
+
+        # Init root and add tender parent
+        root = Root(RequestWithRegistry(self.app.app.registry))
+        test_tender.__parent__ = root
+
+        # get_request_from_root should return RequestWithRegistry instance
+        self.assertIsInstance(get_request_from_root(test_tender),
+                              RequestWithRegistry)
+
+        # Add Tender to bid as parent
+        test_bid = Bid(test_bids[0])
+        test_bid.__parent__ = test_tender
+
+        # get_request_from_root should returb ReqeustWithRegistry instance
+        self.assertIsInstance(get_request_from_root(test_bid),
+                              RequestWithRegistry)
+
+    def test_get_request_from_root_without_request(self):
+        # create tender without reqeust
+        test_tender = Tender(test_tender_data)
+        test_tender.tenderid = "ua-x"
+        test_tender.store(self.db)
+
+        # get_request_from_root should return None
+        self.assertIsNone(get_request_from_root(test_tender))
