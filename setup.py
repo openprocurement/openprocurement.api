20e976a1c1dcf507f12a7fd93c1a789bd16ca653
Andrew Leitsius <leits@quintagroup.com>
2018-03-12 19:06:51 +0200

    Prepare openprocurement.api 2.3.6-sale.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ entry_points = {
 }
 
 setup(name='openprocurement.api',
-      version='2.3.5-sale',
+      version='2.3.6-sale',
       description='openprocurement.api',
       long_description=README,
       classifiers=[
