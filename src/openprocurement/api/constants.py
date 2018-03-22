533ef54683303666be983caa9d34f2aeb0c98071
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-03-22 16:11:36 +0200

    Constant for awarding v2_1

--- src/openprocurement/api/constants.py
+++ src/openprocurement/api/constants.py
@@ -3,9 +3,9 @@ from pkg_resources import get_distribution
 
 AWARDING_OF_PROCUREMENT_METHOD_TYPE = {
     'belowThreshold': 'awarding_1_0',
-    'dgfFinancialAssets': 'awarding_3_0',
-    'dgfOtherAssets': 'awarding_3_0',
-    'dgfInsider': 'awarding_3_0',
+    'dgfFinancialAssets': 'awarding_2_1',
+    'dgfOtherAssets': 'awarding_2_1',
+    'dgfInsider': 'awarding_2_1',
 }
 
 
