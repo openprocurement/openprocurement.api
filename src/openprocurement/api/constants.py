8553be51774a3fc420532e30c21597e9e97e6fe5
Andrew Leitsius <leits@quintagroup.com>
2018-03-22 17:50:33 +0200

    Remove AWARDING_OF_PROCUREMENT_METHOD_TYPE constant

--- src/openprocurement/api/constants.py
+++ src/openprocurement/api/constants.py
@@ -1,14 +1,6 @@
 from logging import getLogger
 from pkg_resources import get_distribution
 
-AWARDING_OF_PROCUREMENT_METHOD_TYPE = {
-    'belowThreshold': 'awarding_1_0',
-    'dgfFinancialAssets': 'awarding_3_0',
-    'dgfOtherAssets': 'awarding_3_0',
-    'dgfInsider': 'awarding_3_0',
-}
-
-
 # Declares what roles can interact with document in different statuses
 STATUS4ROLE = {
     'complaint_owner': ['draft', 'answered'],
