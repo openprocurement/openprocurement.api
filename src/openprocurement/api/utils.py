9f856fabc5a99161026f8e7e1b141f6dc844ccc8
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-03-26 13:06:17 +0300

    Change timezone in calculate_business_date

--- src/openprocurement/api/utils.py
+++ src/openprocurement/api/utils.py
@@ -923,11 +923,13 @@ def decrypt(uuid, name, key):
 
 
 def calculate_business_date(date_obj, timedelta_obj, context=None, working_days=False):
+    date_obj = date_obj.replace(tzinfo=None)
+    calculated_date = None
     if context and 'procurementMethodDetails' in context and context['procurementMethodDetails']:
         re_obj = ACCELERATOR_RE.search(context['procurementMethodDetails'])
         if re_obj and 'accelerator' in re_obj.groupdict():
-            return date_obj + (timedelta_obj / int(re_obj.groupdict()['accelerator']))
-    if working_days:
+            calculated_date = date_obj + (timedelta_obj / int(re_obj.groupdict()['accelerator']))
+    if calculated_date is None and working_days:
         if timedelta_obj > timedelta():
             if date_obj.weekday() in [5, 6] and WORKING_DAYS.get(date_obj.date().isoformat(), True) or WORKING_DAYS.get(date_obj.date().isoformat(), False):
                 date_obj = datetime.combine(date_obj.date(), time(0, tzinfo=date_obj.tzinfo)) + timedelta(1)
@@ -943,8 +945,18 @@ def calculate_business_date(date_obj, timedelta_obj, context=None, working_days=
             date_obj += timedelta(1) if timedelta_obj > timedelta() else -timedelta(1)
             while date_obj.weekday() in [5, 6] and WORKING_DAYS.get(date_obj.date().isoformat(), True) or WORKING_DAYS.get(date_obj.date().isoformat(), False):
                 date_obj += timedelta(1) if timedelta_obj > timedelta() else -timedelta(1)
-        return date_obj
-    return date_obj + timedelta_obj
+        calculated_date = date_obj
+    calculated_date = date_obj + timedelta_obj if calculated_date is None else calculated_date
+    calculated_date = datetime(
+        calculated_date.year,
+        calculated_date.month,
+        calculated_date.day,
+        calculated_date.hour,
+        calculated_date.minute,
+        calculated_date.second,
+        calculated_date.microsecond,
+    )
+    return TZ.localize(calculated_date)
 
 
 def json_body(self):
