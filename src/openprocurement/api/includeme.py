40ea1be1d85cfa7e7361c69d0d144047806174fb
Scandie <grizly.vl@gmail.com>
2018-04-19 17:56:14 +0300

    Improve codeclimate

--- src/openprocurement/api/includeme.py
+++ src/openprocurement/api/includeme.py
@@ -14,12 +14,16 @@ def includeme(config, plugin_config=None):
     config.scan("openprocurement.api.subscribers")
     config.registry.registerAdapter(ContentConfigurator, (IOPContent, IRequest),
                                     IContentConfigurator)
-    config.add_request_method(get_content_configurator, 'content_configurator', reify=True)
+    config.add_request_method(
+        get_content_configurator, 'content_configurator', reify=True
+    )
     config.add_request_method(request_get_now, 'now', reify=True)
     if plugin_config and plugin_config.get('plugins'):
         for name in plugin_config['plugins']:
             configure_plugins(
-                config, {name: plugin_config['plugins'][name]}, 'openprocurement.api.plugins', name
+                config, {name: plugin_config['plugins'][name]},
+                'openprocurement.api.plugins', name
             )
 
-    LOGGER.info("Included openprocurement.api plugin", extra={'MESSAGE_ID': 'included_plugin'})
+    LOGGER.info("Included openprocurement.api plugin",
+                extra={'MESSAGE_ID': 'included_plugin'})
