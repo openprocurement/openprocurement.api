edb22f6b288c3af702d90a179a3ef4bb7caa6ace
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-04-25 17:00:51 +0300

    Dont handle lot.Auction

--- src/openprocurement/api/utils.py
+++ src/openprocurement/api/utils.py
@@ -137,7 +137,7 @@ def error_handler(request, request_params=True):
         params['TENDER_REV'] = request.validated['tender'].rev
         params['TENDERID'] = request.validated['tender'].tenderID
         params['TENDER_STATUS'] = request.validated['tender'].status
-    if 'auction' in request.validated:
+    if 'auction' in request.validated and 'lot' not in request.validated:
         params['AUCTION_REV'] = request.validated['auction'].rev
         params['AUCTIONID'] = request.validated['auction'].auctionID
         params['AUCTION_STATUS'] = request.validated['auction'].status
