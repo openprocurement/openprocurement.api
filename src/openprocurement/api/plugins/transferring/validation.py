32f3895f48d96a1abd20d038106d629bd5f1e776
Scandie <grizly.vl@gmail.com>
2018-06-06 13:16:42 +0300

    Remove redundant acrreditation level validators

--- src/openprocurement/api/plugins/transferring/validation.py
+++ src/openprocurement/api/plugins/transferring/validation.py
@@ -62,22 +62,3 @@ def validate_accreditation_level(request, auction, level_name):
         request.errors.add('body', 'data', err)
         request.errors.status = 403
         return
-
-
-def validate_auction_accreditation_level(request, **kwargs):  # pylint: disable=unused-argument
-    if hasattr(request.validated['auction'], 'transfer_accreditation'):
-        predicate = 'transfer_accreditation'
-    else:
-        predicate = 'create_accreditation'
-    validate_accreditation_level(request, request.validated['auction'], predicate)
-
-
-def validate_bid_accreditation_level(request):
-    validate_accreditation_level(request, request.validated['auction'], 'edit_accreditation')
-
-
-def validate_contract_accreditation_level(request):
-    validate_accreditation_level(request, request.validated['contract'], 'create_accreditation')
-
-
-validate_complaint_accreditation_level = validate_bid_accreditation_level
