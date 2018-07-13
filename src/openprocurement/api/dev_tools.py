6230440fb7ee709b83d3851c75c702dc2234afa4
andrii-shchudlo <andriis@quintagroup.org]>
2018-07-13 14:34:44 +0300

    Create csv_role_create methods

---
+++ src/openprocurement/api/dev_tools.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+
+def get_fields_name(cls):
+    fields_name = set(cls._fields._keys)
+    fields_name.update(cls._serializables.keys())
+    return set(fields_name)
+
+
+def get_roles_from_object(cls):
+    fields_name = get_fields_name(cls)
+    roles_keys = [role_name for role_name in cls._options.roles]
+    roles = {}
+    for role_name in roles_keys:
+        rr = cls._options.roles[role_name]
+        roles[role_name] = set([i for i in list(fields_name) if not rr(i, '')])
+    return roles
+
+
+def create_csv_roles(cls):
+    import csv
+    roles = get_roles_from_object(cls)
+    fields_name = list(get_fields_name(cls))
+    path_role_csv = './openprocurement/frameworkagreement/cfaua/models/submodels/'
+    with open('{0}{1}.csv'.format(path_role_csv, cls.__name__), 'wb') as csvfile:
+        fields_name.insert(0, 'rolename')
+        writer = csv.DictWriter(csvfile, fieldnames=fields_name)
+        writer.writeheader()
+        writer = csv.writer(csvfile)
+        for role_name in roles:
+            row = [role_name]
+            for field_name in fields_name[1:]:
+                if field_name in roles[role_name]:
+                   row.append('1')
+                else:
+                    row.append('')
+            writer.writerow(row)
