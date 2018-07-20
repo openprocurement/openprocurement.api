b56d54c84d62665427922948c16462494762fef9
andrii-shchudlo <andriis@quintagroup.org]>
2018-07-20 10:23:32 +0300

    Add test for isoDuration type

--- src/openprocurement/api/tests/dev_tools.py
+++ src/openprocurement/api/tests/dev_tools.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import isodate
 import os
 import sys
 import unittest
@@ -7,7 +8,7 @@ from schematics.types import BooleanType, StringType
 from schematics.types.serializable import serializable
 from openprocurement.api.roles import RolesFromCsv
 from openprocurement.api.tests.base import BaseWebTest
-from openprocurement.api.models import Model
+from openprocurement.api.models import Model, IsoDurationType
 from openprocurement.api.dev_tools import (
     create_csv_roles,
     get_fields_name,
@@ -61,9 +62,47 @@ class CreateRoleCsvTest(BaseWebTest):
         os.remove('{0}.csv'.format(path_role_csv + TestModel.__name__))
 
 
+class IsoDurationTypeTest(BaseWebTest):
+
+    def test_iso_duration_type(self):
+        type_duration = IsoDurationType()
+        period_str = 'P3Y6M4DT12H30M5S'
+        duration_period = isodate.duration.Duration(4, 45005, 0, years=3, months=6)
+
+        res_to_native = type_duration.to_native(period_str)
+        self.assertEqual(res_to_native.years, 3)
+        self.assertEqual(res_to_native.months, 6)
+        self.assertEqual(res_to_native.days, 4)
+        self.assertEqual(res_to_native.tdelta.seconds, 45005)
+        self.assertEqual(res_to_native.tdelta.total_seconds(), 390605.0)
+        self.assertEqual(res_to_native, duration_period)
+
+        res_to_primitive = type_duration.to_primitive(duration_period)
+        self.assertEqual(res_to_primitive, period_str)
+
+        # Parse with errors
+        with self.assertRaises(Exception) as context:
+            result = type_duration.to_native(duration_period)
+        self.assertEqual(context.exception.message,
+                         'Expecting a string isodate.duration.Duration(4, 45005, 0, years=3, months=6)')
+        with self.assertRaises(Exception) as context:
+            result = type_duration.to_native('Ptest')
+        self.assertEqual(context.exception.message,
+                         "ISO 8601 time designator 'T' missing. Unable to parse datetime string 'test'")
+        with self.assertRaises(Exception) as context:
+            result = type_duration.to_native('P3Y6MW4DT12H30M5S')
+        self.assertEqual(context.exception.message,
+                         "Unrecognised ISO 8601 date format: '3Y6MW4D'")
+        with self.assertRaises(Exception) as context:
+            result = type_duration.to_native('123123123123')
+        self.assertEqual(context.exception.message,
+                         "Unable to parse duration string '123123123123'")
+
+
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(CreateRoleCsvTest))
+    suite.addTest(unittest.makeSuite(IsoDurationTypeTest))
     return suite
 
 
