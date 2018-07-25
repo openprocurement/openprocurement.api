74352107190782f22915dde34d0a14bd2f246ee2
Dmitriy Belyaev <dimka2014@users.noreply.github.com>
2018-07-25 11:39:53 +0300

    Prepare 2.4.19-dp release

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ entry_points = {
 }
 
 setup(name='openprocurement.api',
-      version='2.4.18',
+      version='2.4.19-dp',
       description='openprocurement.api',
       long_description=README,
       classifiers=[
