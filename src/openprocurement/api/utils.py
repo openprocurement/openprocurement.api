bdd1c9077a3260dac3c617c9c516b22bc1c2f582
vitalii-motso <vitalii.motso@gmail.com>
2018-12-11 12:54:17 +0200

    common utility to switch auction status (#408)

--- src/openprocurement/api/utils.py
+++ src/openprocurement/api/utils.py
@@ -1301,6 +1301,23 @@ def search_list_with_dicts(container, key, value):
             return item
 
 
+def log_auction_status_change(request, auction, status):
+    """
+        Log auction status change
+
+        :param request: the request object, for context unpack
+        :param auction: the auction object, in which you want to change the status
+        :param status: the status to which you want to switch
+
+        :returns: True
+    """
+
+    msg = 'Switched auction {} to {}'.format(auction.id, status)
+    context_msg = {'MESSAGE_ID': 'switched_auction_{}'.format(status)}
+    LOGGER.info(msg, extra=context_unpack(request, context_msg))
+    return True
+
+  
 def read_json(filename, file_dir):
     """Read file & deserialize it as JSON"""
     full_filename = os.path.join(file_dir, filename)
