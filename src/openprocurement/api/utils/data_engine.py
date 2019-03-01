6dc22b47ba980db78f67dce22a18a173ec8199d3
Bohdan Dmytriv <dmytriv@quintagroup.org>
2019-03-01 10:12:21 +0200

    Upgrade signatures of DataEngine's methods

--- src/openprocurement/api/utils/data_engine.py
+++ src/openprocurement/api/utils/data_engine.py
@@ -3,8 +3,8 @@
 
 class DataEngine(object):
     
-    def apply_data_on_model(data, changes):
+    def apply_data_on_model(data, model):
         pass
 
-    def create_model(data, model):
+    def create_model(data, model_cls):
         pass
