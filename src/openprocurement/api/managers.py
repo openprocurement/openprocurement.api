6f1984aba37ecd3ef37700af90ed861c9de2bbcb
Bohdan Dmytriv <dmytriv@quintagroup.org>
2019-03-01 10:13:03 +0200

    Introduce ModelManagerBase

--- src/openprocurement/api/managers.py
+++ src/openprocurement/api/managers.py
@@ -23,3 +23,20 @@ class Manager(object):
 
     def delete(self, request):
         raise NotImplementedError
+
+
+class ModelManagerBase(object):
+
+    # DataEngine class
+    DATA_ENGINE_CLS = None
+    MODEL_CLS = None
+
+    def __init__(self, event, data_engine_cls=None):
+        self._event = event
+        self._data_engine = self.DATA_ENGINE_CLS() if not data_engine_cls else data_engine_cls()
+
+    def get_applied_data(self):
+        return self._data_engine.apply_data_on_model(self._event.data, self._event.context)
+
+    def create_model(self):
+        return self._data_engine.create_model(self._event.data, self.MODEL_CLS)
