a1dc73573d5090e7ef0bf14ddc5525b695358a4c
Bohdan Dmytriv <dmytriv@quintagroup.org>
2019-03-04 17:21:08 +0200

    DataEngine MVP

--- src/openprocurement/api/utils/data_engine.py
+++ src/openprocurement/api/utils/data_engine.py
@@ -1,10 +1,41 @@
 # -*- coding: utf-8 -*-
+from openprocurement.api.utils.common import apply_data_patch
 
 
 class DataEngine(object):
-    
-    def apply_data_on_model(data, model):
-        pass
 
-    def create_model(data, model_cls):
-        pass
+    def __init__(self, event):
+        self._event = event
+
+    def apply_data_on_model(self):
+        """Applies event.data on event.context and returns applied data wrapped into invalid model
+        """
+        model_cls = self._event.context.__class__
+
+        initial_data = self._event.context.serialize()
+        updated_model = model_cls(initial_data)  # create new model with same data
+
+        new_patch = apply_data_patch(initial_data, self._event.data)
+        if new_patch:
+            updated_model.import_data(new_patch, partial=True, strict=True)
+        updated_model.__parent__ = self._event.context.__parent__
+        updated_model.validate()
+
+        role = self._event.context.get_role(self._event.auth.role)
+        method = updated_model.to_patch
+
+        updated_filtered_model_data = method(role)
+        updated_filtered_model = model_cls(updated_filtered_model_data)
+
+        return updated_filtered_model
+
+    def create_model(self, data, model):
+        role = 'create'
+        model_cls = self._event.context.__class__
+
+        updated_model = model_cls(self._event.data)
+        updated_model.__parent__ = self._event.context.__parent__
+        updated_model.validate()
+        method = updated_model.serialize
+
+        return method(role)
