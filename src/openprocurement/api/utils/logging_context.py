83a0dd9bab3194528312511dc137b181af17503e
Bohdan Dmytriv <dmytriv@quintagroup.org>
2019-04-16 11:25:40 +0300

    Shorten method names

--- src/openprocurement/api/utils/logging_context.py
+++ src/openprocurement/api/utils/logging_context.py
@@ -8,28 +8,28 @@ class LoggingContext(object):
     def get_context(self):
         return self._context
 
-    def get_context_item(self, key):
+    def get_item(self, key):
         return self._context.get(key)
 
-    def set_context_item(self, key, value):
+    def set_item(self, key, value):
         upper_key = key.upper()
         self._context[upper_key] = value
 
-    def set_context_items(self, items):
+    def set_items(self, items):
         for k, v in items.items():
             self.set_context_item(k, v)
 
-    def to_journal_context(self, message=None):
+    def to_journal_logging_context(self, message=None):
         """Convert to JournalLoggingContext object"""
 
         journal_context = JournalLoggingContext()
         message = message if message else {}
 
         for k, v in self._context.items():
-            journal_context.set_context_item(k, v)
+            journal_context.set_item(k, v)
 
         for k, v in message.items():
-            journal_context.set_context_item(k, v, add_prefix=False)  # message must be added with key unprefixed
+            journal_context.set_item(k, v, add_prefix=False)  # message must be added with key unprefixed
 
         return journal_context
 
@@ -38,7 +38,7 @@ class JournalLoggingContext(LoggingContext):
 
     _JOURNAL_CONTEXT_KEY_PREFIX = 'JOURNAL_'
 
-    def set_context_item(self, key, value, add_prefix=True):
+    def set_item(self, key, value, add_prefix=True):
         if not key.startswith(self._JOURNAL_CONTEXT_KEY_PREFIX):
             j_key = self._JOURNAL_CONTEXT_KEY_PREFIX + key.upper()
             self._context[j_key] = value
